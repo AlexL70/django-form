c29f871ee6f70a212ad4598704bc67884dfeceb9
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-11-08 17:25:14 +0200

    426. Customizing the Admin Interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import JobAppForm
 
-admin.site.register(JobAppForm)
+
+class JobAppFormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email",
+                    "start_date", "occupation")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("start_date", "occupation")
+    ordering = ("last_name", "first_name")
+    readonly_fields = ("first_name", "last_name", "email")
+
+
+admin.site.register(JobAppForm, JobAppFormAdmin)

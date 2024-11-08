26e7ef6c3faac020749b09c5378789e5f6b3b865
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-11-08 16:43:47 +0200

    422. Store the Data in the Database

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .forms import ApplicationForm
+from .models import JobAppForm
 from varname import nameof
 
 
@@ -13,6 +15,11 @@ def index(request: HttpRequest) -> HttpResponse:
             email = form.cleaned_data["email"]
             start_date = form.cleaned_data["start_date"]
             occupation = form.cleaned_data["occupation"]
-        print(
-            f"first_name: {first_name}\nlast_name: {last_name}\nemail: {email}\nstart_date: {start_date}\noccupation: {occupation}")
+            JobAppForm.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                start_date=start_date,
+                occupation=occupation)
+            messages.success(request, "Application submitted successfully!")
     return render(request, "index.html")
